24b702b1e5f0cf19017e9990d045a356ccebbf45
Felipe Cardoso <felipedclc@gmail.com>
2020-05-25 17:07:46 -0300

    Initial commit

---
+++ src/application/program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.PrintService;
+
+public class program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService ps = new PrintService();
+		
+		System.out.println("How many values? ");
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			int value = sc.nextInt();
+			ps.addValue(value);
+		}
+		ps.print();
+		System.out.println("First: " + ps.first());
+		
+		sc.close();
+	}
+
+}
---
+++ src/entities/PrintService.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService {
+
+	private List<Integer> list = new ArrayList<>();
+	
+	public void addValue(int value) {
+		list.add(value);
+	}
+	public Integer first() {
+		if(list.isEmpty()) {
+			throw new IllegalStateException("List is empty");
+		}
+		return list.get(0);
+	}
+	public void print() {
+		System.out.println("[ ");
+		if(!list.isEmpty()) {
+			System.out.println(list.get(0));
+		}
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(", " + list.get(i));
+		}
+		System.out.println(" ]");
+	}
+}
